26a194c4a968ab9ccb180b76bfc317153d0210c5
Gaoussou Koné <gaoussou.ony@gmail.com>
2017-12-24 23:22:14 +0100

    wip update

--- imports/ui/layouts/single/Single.js
+++ imports/ui/layouts/single/Single.js
@@ -12,22 +12,76 @@ class Single extends Component {
   componentDidMount () {
     // Detects clicks everywhere on the screen
     document.addEventListener('dblclick', this.handleClick)
+    document.querySelector('.uuu').addEventListener('click', (event) => {
+			if (event.target.nodeName == 'BUTTON') {
+				this.appendForm()
+			}
+		})
   }
 
   componentWillUnmount () {
     document.removeEventListener('dblclick', this.handleClick)
+    // document.addEventListener('click', this.handleClick)
   }
+
+	appendForm() {
+    const form = document.createElement('FORM');
+		const bookName = document.createElement("input")
+		var author = document.createElement("INPUT");
+		const description = document.createElement("TEXTAREA")
+		var btnUpdate = document.createElement("BUTTON");    
+		var btnCancel = document.createElement("BUTTON");    
+		var tUpdate = document.createTextNode("update");
+		var tCancel = document.createTextNode("cancel");
+
+    var array = ["Science-Fiction", "Drame", "Mystè", "Dystopie"];
+
+		bookName.setAttribute("type", "text");
+		author.setAttribute("type", "text");
+
+		bookName.value = this.props.livre.bookname
+		author.value = this.props.livre.author
+		description.value = this.props.livre.description
+
+    var selectList = document.createElement("select");
+
+		btnUpdate.appendChild(tUpdate);    
+		btnCancel.appendChild(tCancel);    
+
+    form.appendChild(bookName);
+    form.appendChild(selectList);
+    form.appendChild(author);
+    form.appendChild(description);
+    form.appendChild(btnUpdate);
+    form.appendChild(btnCancel);
+
+    //Create and append the options
+    for (var i = 0; i < array.length; i++) {
+      var option = document.createElement("option");
+      option.value = array[i];
+      option.text = array[i];
+      selectList.appendChild(option);
+    }
+		document.querySelector('.single__desc').style.display = 'none';
+		document.querySelector('.update').appendChild(form);
+	}
 
 	handleClick(e) {
 		const target = e.target
 		const input = document.createElement("TEXTAREA")
+		const form = document.createElement('FORM');
+
+		form.appendChild(input)
 		const allowedType = ["P","SPAN"]
 		allowedType.map((el) => {
-			if(el == target.nodeName) {
+			if(el == target.nodeName && !target.changed ) {
+				target.changed = true;
 				input.value = target.innerText
 
 				target.innerText = " "
-				target.appendChild(input)
+				// target.appendChild(input)
+				target.parentNode.appendChild(form)
+				// target.appendChild(form)
 			}
 		})
 	}
@@ -46,7 +100,9 @@ class Single extends Component {
 	        	<p>{this.props.livre.bookname}</p>
 	        	<p>{this.props.livre.author}</p>
 	        	<p>{this.props.livre.description}</p>
+						<button className='uuu'>update cuntent</button>
 	        </div>
+					<div className="update"></div>
 	    </div>
 	);
   }
